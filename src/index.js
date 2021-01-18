587feade67b2a9b58407bd30a9b709b3b56b80ab
Tatiana Botia <tatianabb21@gmail.com>
2021-01-18 13:31:52 -0500

    configuraciones

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import HelloWorld from './Components/HelloWolrd'
+
+ReactDOM.render(
+    <HelloWorld/>,
+    document.getElementById('app')
+)
+
